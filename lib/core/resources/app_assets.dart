92c751668b061cc7f32872fa6a575f5fa0c31816
Ahmed <elzek989@gmail.com>
2025-07-14 03:24:03 +0300

    init assets

---
+++ lib/core/resources/app_assets.dart
@@ -0,0 +1,18 @@
+const String imagePath = "assets/images";
+const String svgPath = "assets/svg_images";
+
+abstract class ImageAssets {
+  static const String routeLogo = "$imagePath/route_logo.png";
+  static const String splashBackground = "$imagePath/splash_background.png";
+}
+
+abstract class SvgAssets {
+  static const String icHome = "$svgPath/ic_home.svg";
+  static const String icCart = "$svgPath/ic_cart.svg";
+  static const String icHeart = "$svgPath/ic_heart.svg";
+  static const String icSearch = "$svgPath/ic-search.svg";
+  static const String icProfile = "$svgPath/ic_profile.svg";
+  static const String icDelete = "$svgPath/ic_delete.svg";
+  static const String icCategory = "$svgPath/ic_category.svg";
+  static const String icAdd = "$svgPath/ic_add.svg";
+}
